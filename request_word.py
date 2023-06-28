074b7b13ef7818407a711c71304ad7ce2abceb88
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-06-28 09:47:44 +0800

    batch selection

--- request_word.py
+++ request_word.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from os import system
-from random import choice
+from random import choice, random
 from sys import argv
 from time import sleep
 
@@ -63,6 +63,18 @@ def select_words(words: dict[str, str], no_dep=True):
     return select_from
 
 
+def batch_words(
+    words: list[WordEntry], batch_size: int, open_v: bool, open: bool, delay: int
+):
+    stay_probability = 1.5**batch_size
+    select_from = [choice(words) for _ in range(batch_size)]
+    last_selected = None
+    while random() < stay_probability:
+        stay_probability /= 1.5
+        select_this_round = [w for w in select_from if w != last_selected]
+        last_selected = pop_word(select_this_round, open_v, open, delay)
+
+
 def open_dict(word: str):
     to_run = f"""osascript -e 'open location "dict://{word}"'"""
     if system(to_run) != 0:
@@ -82,21 +94,39 @@ def get_delay(argv):
     return 5 * 60
 
 
+def get_get_batch_size(argv):
+    for arg in argv:
+        if arg.startswith("--batch="):
+            batch_size = int(arg.split("=")[1])
+            assert batch_size > 1
+            return batch_size
+    return None
+
+
+def pop_word(words: list[WordEntry], open_v: bool, open: bool, delay: int):
+    word = choice(words)
+    notify(word.meaning, word.word)
+    if open_v:
+        open_voca(word.word)
+    if open:
+        open_dict(word.word)
+    sleep(delay)
+    return word
+
+
 def main():
     open = any(arg == "--open" for arg in argv)
     open_v = any(arg == "--open-voca" for arg in argv)
     no_dep = any(arg == "--no-dep" for arg in argv)
+    batch_size = get_get_batch_size(argv)
     words = read_words("word_list.txt")
     words = select_words(words, no_dep)
     delay = get_delay(argv)
     while 1:
-        word = choice(words)
-        notify(word.meaning, word.word)
-        if open_v:
-            open_voca(word.word)
-        if open:
-            open_dict(word.word)
-        sleep(delay)
+        if batch_size is None:
+            pop_word(words, open_v, open, delay)
+        else:
+            batch_words(words, batch_size, open_v, open, delay)
 
 
 try:
